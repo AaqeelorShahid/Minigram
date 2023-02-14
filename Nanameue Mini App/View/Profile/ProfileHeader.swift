9272826849b4dc7696027910233f06286d0c741f
shahid.aaqeel <aaqeelzakeer0@gmail.com>
2023-02-14 21:36:52 +0530

    profile controller ui improvement

--- Nanameue Mini App/View/Profile/ProfileHeader.swift
+++ Nanameue Mini App/View/Profile/ProfileHeader.swift
@@ -11,7 +11,7 @@ import UIKit
 class ProfileHeader: UICollectionReusableView {
     // MARK: - Properties
     let profilePicture: UIButton = {
-        let button = UIButton()
+        let button = UIButton(type: .custom)
         button.setImage(UIImage(named: "profile"), for: .normal)
         button.imageView?.contentMode = .scaleToFill
         return button
@@ -60,7 +60,6 @@ class ProfileHeader: UICollectionReusableView {
         return label
     }()
     
-    
     private lazy var editProfileBtn: UIButton = {
         let button = UIButton(type: .system)
         button.backgroundColor = UIColor(named: "main_color")
@@ -69,7 +68,34 @@ class ProfileHeader: UICollectionReusableView {
         button.titleLabel?.font = UIFont.systemFont(ofSize: 15)
         button.layer.cornerRadius = 15
         button.setHeight(40)
+        button.isUserInteractionEnabled = true
         button.addTarget(self, action: #selector(editProfileBtnPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var likeSectionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "hand.thumbsup"), for: .normal)
+        button.setTitle("Likes", for: .normal)
+        button.tintColor = UIColor.black
+        button.setTitleColor(.black, for: .normal)
+        button.imageView?.contentMode = .scaleToFill
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 20)
+        button.isUserInteractionEnabled = true
+        button.addTarget(self, action: #selector(postSectionBtnPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var postSectionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "square.on.square.intersection.dashed"), for: .normal)
+        button.setTitle("Posts", for: .normal)
+        button.tintColor = UIColor.black
+        button.setTitleColor(.black, for: .normal)
+        button.imageView?.contentMode = .scaleToFill
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 20)
+        button.isUserInteractionEnabled = true
+        button.addTarget(self, action: #selector(likeSectionBtnPressed), for: .touchUpInside)
         return button
     }()
     
@@ -80,6 +106,15 @@ class ProfileHeader: UICollectionReusableView {
         imageView.backgroundColor = .systemGray6
         return imageView
     }()
+    
+    private let separatorViewBottom: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .systemGray6
+        return imageView
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
@@ -117,6 +152,14 @@ class ProfileHeader: UICollectionReusableView {
         addSubview(separatorView)
         separatorView.anchor(top: editProfileBtn.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 16, height: 1)
         
+        let profilSectionStack = UIStackView(arrangedSubviews: [likeSectionButton, postSectionButton])
+        addSubview(profilSectionStack)
+        profilSectionStack.distribution = .fillEqually
+        profilSectionStack.anchor(top: separatorView.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 12)
+        
+        addSubview(separatorViewBottom)
+        separatorViewBottom.anchor(top: profilSectionStack.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 12, height: 1)
+        
     }
     
     required init?(coder: NSCoder) {
@@ -136,6 +179,14 @@ class ProfileHeader: UICollectionReusableView {
 //    MARK: - Actions
     
     @objc func editProfileBtnPressed() {
-        
+     print("asdasd")
+    }
+    
+    @objc func postSectionBtnPressed() {
+        print("asdasd")
+    }
+    
+    @objc func likeSectionBtnPressed() {
+        print("asdasd")
     }
 }
